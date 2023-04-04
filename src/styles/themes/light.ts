4ca561e492f00aab742cf9457ef496fdf2c845c0
Hugo Hoffmann <hugoalvesdesolza@gmail.com>
2023-04-04 12:33:00 -0300

    feat: Light theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,17 @@
+export default {
+  title: 'light',
+  colors: {
+    primary: '#F15156',
+    secundary: '#E44449',
+    background: '#FDECED',
+    auth_bg: '#ffffff',
+    button_primary_bg: '#0D3B66',
+    button_primary_label: '#ffffff',
+    button_secundary_bg: '#f7f7f7',
+    button_secundary_label: '#0D3B66',
+    label_primary: '#0D3B66',
+    input_primary_bg: '#F5F8FA',
+    input_primary_border: '#D3E2E5',
+    input_primary_value: '#0D3B66',
+  },
+}

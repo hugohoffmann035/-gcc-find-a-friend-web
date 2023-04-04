dedf52715b308905a42e4ed33dae9d6d058c322e
Hugo Hoffmann <hugoalvesdesolza@gmail.com>
2023-04-04 12:34:30 -0300

    feat: Added theme provider; function for change current theme; save and get setup theme from and to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
+import { useLayoutEffect, useState } from 'react'
 import { Router } from './routes'
 import { BrowserRouter } from 'react-router-dom'
-import GlobalStyles from './styles/global'
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyles from '@/styles/global'
+import light from '@/styles/themes/light'
+import dark from '@/styles/themes/dark'
 
 function App() {
+  const [theme, setTheme] = useState(light)
+
+  const toggleTheme = () => {
+    const newTheme = theme.title === 'light' ? dark : light
+    setTheme(newTheme)
+    localStorage.setItem('theme', newTheme.title)
+  }
+
+  useLayoutEffect(() => {
+    const themeStorage = localStorage.getItem('theme') ?? 'light'
+    const newTheme = themeStorage === 'light' ? light : dark
+    setTheme(newTheme)
+  }, [])
+
   return (
-    <>
+    <ThemeProvider theme={light}>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
       <GlobalStyles />
-    </>
+    </ThemeProvider>
   )
 }
 

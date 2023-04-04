c73906da8bb2101babb6ab4991ba741d9571210e
Hugo Hoffmann <hugoalvesdesolza@gmail.com>
2023-04-04 12:32:12 -0300

    feat: Added type for theming

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,21 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    title: string
+    colors: {
+      primary: string
+      secundary: string
+      background: string
+      auth_bg: string
+      button_primary_bg: string
+      button_primary_label: string
+      button_secundary_bg: string
+      button_secundary_label: string
+      label_primary: string
+      input_primary_bg: string
+      input_primary_border: string
+      input_primary_value: string
+    }
+  }
+}

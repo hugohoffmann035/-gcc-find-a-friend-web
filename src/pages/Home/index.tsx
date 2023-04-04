7007dcba303e5870908e7aa12581e3d8284abdb9
Hugo Hoffmann <hugoalvesdesolza@gmail.com>
2023-04-04 12:34:57 -0300

    feat: More components to show in the home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,17 @@
-import { Container } from './styles'
+import {
+  Container,
+  Wrapper,
+  Logo,
+  Banner,
+  Action,
+  ButtonSelectState,
+  ButtonSelectCity,
+  ButtonSearch,
+} from './styles'
+import LogoSvg from '@/assets/icons/logo.svg'
+import DogsSvg from '@/assets/images/dogs.svg'
+import ArrowDownSvg from '@/assets/icons/arrow-down.svg'
+import SearchSvg from '@/assets/icons/search.svg'
 
 export function Home() {
   function handleSearchPets() {
@@ -13,5 +26,47 @@ export function Home() {
     // TO DO
   }
 
-  return <Container>{/* COMECE POR AQUI */}</Container>
+  return (
+    <Container>
+      <Wrapper>
+        <Logo>
+          <img src={LogoSvg} />
+          <span className="name">FindAFriend</span>
+        </Logo>
+        <Banner>
+          <div className="copy">
+            <p>Leve</p>
+            <p>a felicidade</p>
+            <p>para o seu lar</p>
+          </div>
+          <div>
+            <img src={DogsSvg} />
+          </div>
+        </Banner>
+        <Action>
+          <div className="call-to-action">
+            <p>Encontre o animal de estimação ideal</p>
+            <p>para seu estilo de vida!</p>
+          </div>
+          <form className="form">
+            <span className="label">Busque um amigo:</span>
+            <div className="separator" />
+            <ButtonSelectState>
+              <span>PE</span>
+              <img src={ArrowDownSvg} />
+            </ButtonSelectState>
+            <div className="separator" />
+            <ButtonSelectCity>
+              <span>Recife</span>
+              <img src={ArrowDownSvg} />
+            </ButtonSelectCity>
+            <div className="separator" />
+            <ButtonSearch>
+              <img src={SearchSvg} />
+            </ButtonSearch>
+          </form>
+        </Action>
+      </Wrapper>
+    </Container>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,133 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  background: ${(props) => props.theme.colors.primary};
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1216px;
+  max-width: 90%;
+`
+
+export const Logo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  img {
+  }
+
+  .name {
+    margin-left: 0.5rem;
+    font-weight: 700;
+    font-size: 1.5rem;
+  }
+`
+
+export const Banner = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 4rem;
+
+  align-items: center;
+
+  .copy {
+    p {
+      font-size: 4rem;
+      font-weight: 800;
+      line-height: 4rem;
+    }
+  }
+
+  .media {
+  }
+`
+
+export const Action = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 4rem;
+
+  .call-to-action {
+    p {
+      font-size: 1.2rem;
+      font-weight: 600;
+      line-height: 1.6rem;
+    }
+  }
+
+  .form {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    .label {
+      font-size: 1rem;
+    }
+
+    .separator {
+      margin: 0px 0.4rem;
+    }
+  }
+`
+
+export const ButtonSelectState = styled.button`
+  height: 72px;
+  width: 72px;
+  border: 1px solid #fff;
+  border-radius: 20px;
+  background: transparent;
+  color: #fff;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 1.2rem;
+  font-weight: 700;
+
+  img {
+    margin-left: 6px;
+  }
+`
+
+export const ButtonSelectCity = styled.button`
+  height: 72px;
+  min-width: 280px;
+  border-radius: 20px;
+  background: ${(props) => props.theme.colors.secundary};
+  border-style: none;
+  color: #fff;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 1.2rem;
+  font-weight: 700;
+
+  img {
+    margin-left: 6px;
+  }
+`
+
+export const ButtonSearch = styled.button`
+  height: 72px;
+  min-width: 72px;
+  border-radius: 20px;
+  border-style: none;
+  background: #f4d35e;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
